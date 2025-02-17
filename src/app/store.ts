dbeedc289960bce3344c3dc4c2d8623684245be5
Ruchika30 <sood.ruchika@siesgst.ac.in>
2025-02-17 13:12:51 +0530

    removed unwanted files

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,10 +1,8 @@
 import { configureStore, ThunkAction, Action } from "@reduxjs/toolkit"
-import counterReducer from "../features/counter/counterSlice"
 import userReducer from "../features/campaigns/campaignSlice"
 
 export const store = configureStore({
 	reducer: {
-		counter: counterReducer,
 		users: userReducer
 	}
 })
